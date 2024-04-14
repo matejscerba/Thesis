eab9f4c89cb8d53f5c0f921c178039abbec4345a
Matěj Ščerba <m.scerba@seznam.cz>
2024-04-14 16:13:45 +0200

    statements recommenders added

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,5 @@
+from typing import Dict, Set, Optional
+
 import pandas as pd
 
 
@@ -5,3 +7,35 @@ class DataLoader:
     @classmethod
     def load(cls) -> pd.DataFrame:
         return pd.read_csv("data/laptops_numerical_with_price.csv", delimiter=";")
+
+    @classmethod
+    def get_ids_for_value(cls, data: pd.DataFrame) -> Dict[str, Dict[float, Set[int]]]:
+        ids_for_value = {}
+
+        for column in data:
+            if column not in ["id", "Unnamed: 0"]:
+                unique_values = data[column].unique()
+                ids_for_value[column] = {}
+                for value in unique_values:
+                    if pd.isna(value):
+                        ids_for_value[column][value] = set(data[data[column].isna()]["id"])
+                    else:
+                        ids_for_value[column][value] = set(data[data[column] == value]["id"])
+
+        return ids_for_value
+
+    @classmethod
+    def get_ids_for_value_compact(cls, data: pd.DataFrame) -> Dict[str, Set[int]]:
+        ids_for_value = {}
+
+        for column in data:
+            if column not in ["id", "Unnamed: 0"]:
+                unique_values = data[column].unique()
+                for value in unique_values:
+                    column_value = f"{column}:{value}"
+                    if pd.isna(value):
+                        ids_for_value[column_value] = set(data[data[column].isna()]["id"])
+                    else:
+                        ids_for_value[column_value] = set(data[data[column] == value]["id"])
+
+        return ids_for_value
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import json
 
 from recommenders.similarity import SimilarityRecommender
+from recommenders.statements_simple import StatementsSimpleRecommender
 
 if __name__ == "__main__":
-    print(json.dumps(SimilarityRecommender.predict(), indent=4, ensure_ascii=False))
+    # Recommender = SimilarityRecommender
+    Recommender = StatementsSimpleRecommender
+
+    print(json.dumps(Recommender.predict(), indent=4, ensure_ascii=False))
