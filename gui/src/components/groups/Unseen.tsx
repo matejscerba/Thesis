7cbf014435418bf70ed6259d9b86b42c5d91b81b
Matěj Ščerba <m.scerba@seznam.cz>
2024-07-18 16:07:16 +0200

    wip

--- gui/src/components/groups/Unseen.tsx
+++ gui/src/components/groups/Unseen.tsx
@@ -1,8 +1,8 @@
 import Typography from "@mui/material/Typography";
 import React from "react";
-// import NumericalAttributeRange from "../attributes/NumericalAttributeRange";
+import NumericalAttributeRange from "../attributes/NumericalAttributeRange";
 import CategoricalAttributeRange from "../attributes/CategoricalAttributeRange";
-// import { AttributeType } from "../../types/attribute";
+import { AttributeType } from "../../types/attribute";
 import { useCategory } from "../../contexts/category";
 
 /**
@@ -21,25 +21,25 @@ function Unseen() {
       {unseen.attributes && unseen.attributes.length > 0 ? (
         unseen.attributes.map((stats) => (
           <div key={stats.attribute.name} className="m-3 border border-secondary rounded bg-white">
-            {/*{stats.attribute.type === AttributeType.CATEGORICAL.valueOf() && (*/}
-            <CategoricalAttributeRange
-              attribute={stats.attribute}
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-              selectedOptions={stats.selected_options ?? []}
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-              availableOptions={stats.available_options ?? stats.options}
-              numProductsInRange={stats.num_products}
-            />
-            {/*)}*/}
-            {/*{stats.attribute.type === AttributeType.NUMERICAL.valueOf() && (*/}
-            {/*  <NumericalAttributeRange*/}
-            {/*    attribute={stats.attribute}*/}
-            {/*    lowerBoundIndex={stats.lower_bound_index}*/}
-            {/*    upperBoundIndex={stats.upper_bound_index}*/}
-            {/*    options={stats.options}*/}
-            {/*    numProductsInRange={stats.num_products}*/}
-            {/*  />*/}
-            {/*)}*/}
+            {stats.attribute.type === AttributeType.CATEGORICAL.valueOf() && (
+              <CategoricalAttributeRange
+                attribute={stats.attribute}
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                selectedOptions={stats.selected_options ?? []}
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                availableOptions={stats.available_options ?? stats.options}
+                numProductsInRange={stats.num_products}
+              />
+            )}
+            {stats.attribute.type === AttributeType.NUMERICAL.valueOf() && (
+              <NumericalAttributeRange
+                attribute={stats.attribute}
+                lowerBoundIndex={stats.lower_bound_index}
+                upperBoundIndex={stats.upper_bound_index}
+                options={stats.options}
+                numProductsInRange={stats.num_products}
+              />
+            )}
           </div>
         ))
       ) : (
